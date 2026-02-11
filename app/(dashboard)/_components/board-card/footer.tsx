f6d4eefaad2ce03532e16dfda444debb6267de8d
KOUSHIK SARKAR <koushiksarkar7102@gmail.com>
2026-02-11 19:07:48 +0530

    [ADD] Board Card favorite functionality

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -10,7 +10,14 @@ interface FooterProps {
   disabled: boolean;
 }
 export const Footer = ({isFavorite,title,authorLabel,createdAtLabel,disabled, onClick}:FooterProps) => {
-  return (
+    const handleClick = (
+        event :React.MouseEvent<HTMLButtonElement,MouseEvent>
+    )=>{
+        event.stopPropagation();
+        event.preventDefault();
+        onClick();
+    }
+    return (
     <div className="relative bg-white p-3">
         <p className="text-[13px] truncate max-w-[calc(100%-20px)]">
             {title}
@@ -21,7 +28,7 @@ export const Footer = ({isFavorite,title,authorLabel,createdAtLabel,disabled, on
 
         <button
             disabled={disabled}
-            onClick={onClick}
+            onClick={handleClick}
             className={cn("opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600", disabled && "cursor-not-allowed opacity-75")}
             >
             <Star
--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -9,6 +9,9 @@ import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Actions } from "@/components/actions";
 import { MoreHorizontal } from "lucide-react";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 interface boardCardProps {
   id: string;
@@ -36,7 +39,18 @@ export const BoardCard = ({
   const createdAtLabel = formatDistanceToNow(createdAt, {
     addSuffix: true,
   });
+  const {mutate:onFavorite,pending:pendingFavorite} = useApiMutation(api.board.favorite);
+  const {mutate:onUnFavorite,pending:pendingUnFavorite} =useApiMutation(api.board.unfavorite)
 
+  const toggleFavorite = ()=>{
+    if(isFavorite){
+      onUnFavorite({id})
+        .catch(()=>toast.error("Failed to unfavorite"))
+    }else{
+      onFavorite({id,orgId})
+        .catch(()=>toast.error("Failed to favorite"))
+    }
+  }
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
@@ -55,8 +69,8 @@ export const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnFavorite}
         />
       </div>
     </Link>
